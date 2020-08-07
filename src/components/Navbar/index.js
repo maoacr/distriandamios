f01ef7fd0c3eeba3dfef361fa1aef351c99469e9
Mario A. Crespo Reyes <maoacr@Marios-iMac.local>
2020-08-07 00:49:18 -0500

    Working on Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import { Menu, MenuItems } from './styles'
+import { Menu, Hamburguer, MenuItems, Logotipo } from './styles'
 
+import HambMenu from '../../assets/hamburguer.svg';
+import Logo from '../../assets/logo.svg';
 
 const Navbar = () => {
     return(
         <Menu>
-            <img src='../../assets/logo.svg' alt='Logo de Distriandamios'/>
+            <Logotipo src={Logo} alt='Logo de Distriandamios'/>
             <MenuItems>
                 <li>Quienes somos ?</li>
                 <li>Productos y servicios</li>
                 <li>Contacto</li>
             </MenuItems>
+            <Hamburguer src={HambMenu} alt='Menu  hamburguesa'></Hamburguer>
         </Menu>
     )
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -3,10 +3,22 @@ import styled from 'styled-components';
 export const Menu = styled.nav`
     display: flex;
     justify-content: space-between;
-    width: 100%;
-    height: 60px;
+    /* width: 100%; */
+    height: 80px;
+    align-items: center;
+    padding-left: 30px;
+    padding-right: 30px;
 `
 
 export const MenuItems = styled.ul`
     display: none;
+`
+
+export const Logotipo = styled.img`
+    width: 40px;
+    height: 40px;
+`
+export const Hamburguer = styled.img`
+    width: 40px;
+    height: 40px;
 `
