2b104b11844f5e63bd2e2c4206621cf648f5a2e6
Beto <betotoro0902@gmail.com>
2020-08-11 17:15:12 -0500

    Functionality of the hamburguer menú

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Menu, Hamburguer, MenuItems, Logotipo, ListItems, Link } from './styles'
+import {
+    Menu,
+    Hamburguer,
+    MenuItems,
+    MenuItems2,
+    Logotipo,
+    ListItems,
+    ListItems2,
+    Link
+} from "./styles"
 
 import HambMenu from '../../assets/hamburguer.svg';
 import Logo from '../../assets/logo.svg';
 
 const Navbar = () => {
-
+    const [menu, setMenu] = useState(true)
+    const handleMenu = () => {
+        setMenu(!menu)
+    }
     return(
         <Menu>
-            <Logotipo src={Logo} alt='Logo de Distriandamios' />
-            <MenuItems>
-                <ListItems><Link href='/distriandamios'>Quienes somos</Link></ListItems>
-                <ListItems><Link href='/productos-y-servicios'>Productos y servicios</Link></ListItems>
-                <ListItems><Link href='/contact'>Contacto</Link></ListItems>
-            </MenuItems>
-            <Hamburguer src={HambMenu} alt='Menu  hamburguesa'></Hamburguer>
+            <Logotipo src={Logo} onClick={handleMenu} alt='Logo de Distriandamios' />
+            {
+                menu
+                    ? (
+                        <MenuItems>
+                            <ListItems><Link href='/distriandamios'>Quienes somos</Link></ListItems>
+                            <ListItems><Link href='/productos-y-servicios'>Productos y servicios</Link></ListItems>
+                            <ListItems><Link href='/contact'>Contacto</Link></ListItems>
+                        </MenuItems>
+                    )
+                    : (
+                        <MenuItems2>
+                            <ListItems><Link href='/distriandamios'>Quienes somos</Link></ListItems>
+                            <ListItems><Link href='/productos-y-servicios'>Productos y servicios</Link></ListItems>
+                            <ListItems><Link href='/contact'>Contacto</Link></ListItems>
+                        </MenuItems2>
+                    )
+            }
+
+            <Hamburguer onClick={handleMenu} src={HambMenu} alt='Menu  hamburguesa'></Hamburguer>
         </Menu>
     )
 }
