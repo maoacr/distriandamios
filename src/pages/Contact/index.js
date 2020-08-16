e59b467e09acb7ba43ae16727aa9af2112a439d1
Mario A. Crespo Reyes <maoacr@Marios-iMac.local>
2020-08-15 23:00:47 -0500

    Contact us page structure ready

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, IlustracionWrapper, Ilustracion, Distriandamios  } from './styles';
+import { Layout, IlustracionWrapper, Ilustracion, Distriandamios, LastSection, GuardarContacto, Enunciado, Correo } from './styles';
 
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
@@ -11,6 +11,8 @@ import Puntos from '../../components/Puntos';
 import ContactUs from '../../assets/contactUs.svg';
 
 import Contactanos from '../../assets/contactanos.svg';
+
+const ContactFile = '../../assets/'
 
 const Contact = () => {
     return(
@@ -26,6 +28,13 @@ const Contact = () => {
             <Divider />
             <Puntos />
             <Divider />
+            <LastSection>
+                <Enunciado>Si tienes solicitudes, dudas o inquietudes, puedes escribirnos o llamarnos aquí:</Enunciado>
+                <Correo href="mailto:distriandamios@gmail.com" >distriandamios@gmail.com</Correo>
+                <h2><strong>PBX :</strong> 786 84 95</h2>
+                <Enunciado>Guarda un contácto en tu teléfono con todos nuestros datos como dirección, página web y PBX en un solo click</Enunciado>
+                <a href={ContactFile} download="Distriandamios SAS"><GuardarContacto>Guardar Contacto</GuardarContacto></a>
+            </LastSection>
             <Footer />
         </Layout>
     );
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { Colors } from '../../utils/Constants';
+
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
@@ -41,4 +43,34 @@ export const Distriandamios = styled.img`
     position: absolute;
     top: 30%;
   }
+`
+export const LastSection = styled.div`
+  width: 100%; 
+  max-width: 700px;
+  margin: 0 auto;
+  margin-bottom: 100px;
+`
+export const GuardarContacto = styled.button`
+  width: 90%;
+  max-width: 220px;
+  border: none;
+  padding: 16px 32px;
+  background-color: ${Colors.mainRed};
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 20px 0;
+`
+export const Enunciado = styled.h3`
+  margin: 40px 0;
+  color: ${Colors.grayColor};
+  font-weight: lighter;
+`
+export const Correo = styled.a`
+  text-decoration: none;
+  font-size: 24px;
+  font-weight: bold;
+  color: black;
 `
